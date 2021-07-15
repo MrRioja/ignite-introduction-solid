5e299a280a563e989d3d4aa75bc00d8596c6e848
Luiz Rioja <lulyrioja@gmail.com>
2021-07-15 14:28:49 -0300

    :alien: Creating the ListAllUser controller and useCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    let all = [];
+
+    try {
+      all = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString(),
+      });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+
+    return response.json(all);
   }
 }
 
